7ae8472504bedb6eaa153939379b8230e9418071
sameekshasam2006 <you@example.com>
2026-05-18 20:07:22 +0530

    fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: "K's Showrooms",
-        short_name: "Showroom",
-        description: "Smart Inventory + Online Shop",
-        theme_color: "#0f172a",
-        background_color: "#0f172a",
-        display: "standalone",
-        icons: [
-          {
-            src: "/icon.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "/icon.png",
-            sizes: "512x512",
-            type: "image/png"
-          }
-        ]
-      }
-    })
-  ]
+  plugins: [react()],
 })
